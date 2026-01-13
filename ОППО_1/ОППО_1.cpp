79c11a5fd8a1e27f126595322b0494b5fb5944f5
Yaka-tea <timafuntime@gmail.com>
2026-01-13 21:16:34 +0700

    Initial commit: practical work 1

---
+++ ОППО_1/ОППО_1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+struct GoodsArrival {
+    string date;
+    string name;
+    int quantity;
+};
+
+int main() {
+    setlocale(LC_ALL, "RU");    
+    cout << "Введите строку в формате:" << endl;
+    cout << "гггг.мм.дд \"Название товара\" количество" << endl;
+
+    string input;
+    getline(cin, input);
+
+    stringstream ss(input);
+
+    GoodsArrival arrival;
+
+    ss >> arrival.date;
+    ss >> ws;
+    getline(ss, arrival.name, '"');
+    getline(ss, arrival.name, '"');
+    ss >> arrival.quantity;
+
+    cout << endl;
+    cout << "Результат разбора строки:" << endl;
+    cout << "Дата: " << arrival.date << endl;
+    cout << "Товар: " << arrival.name << endl;
+    cout << "Количество: " << arrival.quantity << endl;
+
+    return 0;
+}
